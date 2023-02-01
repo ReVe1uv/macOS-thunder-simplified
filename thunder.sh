a3b99ea85bf17b6ec6d1976df425cc5a14c97df5
Wang Qiang <736242642@qq.com>
2023-02-01 13:54:33 +0800

    Update thunder.sh

--- thunder.sh
+++ thunder.sh
@@ -1,8 +1,3 @@
 #/bin/zsh
 cd /Applications/Thunder.app/Contents/PlugIns
 ls | grep -v 'Thunder Extension.appex\|myvip.xlplugin\|applications.xlplugin\|preferences.xlplugin\|browserhelper.xlplugin\|searchtask.xlplugin\|details.xlplugin\|userlogin.xlplugin\|diagnostic.xlplugin\|viptask.xlplugin\|downloadhistory.xlplugin\|viptips.xlplugin' | xargs rmtrash
-
-webrtc\src\webrtc\typedefs.h
-defined(__loongarch64__)
-#define WEBRTC_ARCH_64_BITS
-#define WEBRTC_ARCH_LITTLE_ENDIAN
